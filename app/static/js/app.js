73e8b2a7a4140ec7a7a193fea4cbdad09a66ddce
aizeek1 <Keekopurple@yahoo.com>
2017-03-25 15:15:00 +0000

    added javascript code, sidebar pages load onto home page when clicked

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,1 +1,10 @@
-alert('working');
+/* global $ */
+$(document).ready(function(){
+    //handle sidebar clicks
+    $('ul#sidenav li a').click(function(){
+        var page = $(this).attr('href');
+        //alert(page);
+        $('#sidecontent').load(page);
+        return false;
+    });
+});
